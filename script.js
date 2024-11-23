d71f525fcf8a8b2e9fa1abcdbede0971634743db
Saksham Jaiswal <116110942+saksham-jais@users.noreply.github.com>
2024-11-23 21:46:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    const fadeInElement = document.querySelector('header');
+    fadeInElement.classList.add('visible'); // Trigger the fade-in transition
+    const Scle = document.querySelector('.scl');
+    Scle.classList.add('visible');
+    const Slde = document.querySelector('.slide');
+    Slde.classList.add('visible');
+  
+  };
